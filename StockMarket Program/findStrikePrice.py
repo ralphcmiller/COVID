b15618b8b54e7abad9fdcaa93c31ef62392f3fe5
ralphcmiller <ralphcmille@gmail.com>
2020-03-21 23:30:33 -0700

    update
    Completed the webscraping features to find the current stock price,  find an options strike price, and the value of the option contract at the given strike price

---
+++ StockMarket Program/findStrikePrice.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 21:01:44 2020
+
+@author: Ralph
+"""
+import time
+import bs4
+import requests
+from bs4 import BeautifulSoup
+
+strikePrice = 0
+input_strike = '160.00'
+counter = -1
+## Find Strike Price 
+while strikePrice!=input_strike:
+    counter = counter + 1
+    r = requests.get('https://finance.yahoo.com/quote/SPY/options?p=SPY')
+    soup = bs4.BeautifulSoup(r.text,"lxml")
+    strikePrice = soup.find_all('a',{'class': 'C($linkColor) Fz(s)'})[counter].text
+else:
+    print ('')
+    print (strikePrice)
+    print (counter)
+
+#find Option last value
+
+    r = requests.get('https://finance.yahoo.com/quote/SPY/options?p=SPY')
+    soup = bs4.BeautifulSoup(r.text,"lxml")
+    optionPrice = soup.find_all('td',{'class': 'data-col3 Ta(end) Pstart(7px)'})[counter].text
+    print (optionPrice)
--- StockMarket Program/webScraper.py
+++ StockMarket Program/webScraper.py
@@ -10,6 +10,9 @@ import requests
 from bs4 import BeautifulSoup
 
 starttime=time.time()
+strikePrice = 0
+input_strike = '160.00'
+counter = -1
 
 
 def getPrice():
@@ -20,14 +23,23 @@ def getPrice():
     return price
 #    r.close()
     
+def getStrike():
+    strikePrice = 0
+    counter = -1
+    while strikePrice!=input_strike:
+        counter = counter + 1
+        r = requests.get('https://finance.yahoo.com/quote/SPY/options?p=SPY')
+        soup = bs4.BeautifulSoup(r.text,"lxml")
+        strikePrice = soup.find_all('a',{'class': 'C($linkColor) Fz(s)'})[counter].text
+    else:
+         r = requests.get('https://finance.yahoo.com/quote/SPY/options?p=SPY')
+         soup = bs4.BeautifulSoup(r.text,"lxml")
+         optionPrice = soup.find_all('td',{'class': 'data-col3 Ta(end) Pstart(7px)'})[counter].text
+         return strikePrice,optionPrice
+
 while True:
-    
+    a, b = getStrike()
     print ('The current SPY price is: ' + str(getPrice()))
+    print ('The current SPY strike price is: ' + str(getStrike()))
+    print ('The current SPY option price for this strike is: ' + str(getStrike()))    
     time.sleep(30.0 - ((time.time() - starttime) % 30.0))
-  
-
-
-
-
-## def getOption():
-    
