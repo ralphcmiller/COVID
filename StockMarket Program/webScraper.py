466f6820f4cacdb48733c47bcf59fdc596072531
ralphcmiller <ralphcmille@gmail.com>
2020-03-22 00:07:12 -0700

    efficiency update
    made the program more efficient by only grabbing the webpage twice every computation

--- StockMarket Program/webScraper.py
+++ StockMarket Program/webScraper.py
@@ -11,35 +11,28 @@ from bs4 import BeautifulSoup
 
 starttime=time.time()
 strikePrice = 0
-input_strike = '160.00'
+input_strike = '170.00'
 counter = -1
 
 
 def getPrice():
     
+    strikePrice = 0
+    counter = -1
     r = requests.get('https://finance.yahoo.com/quote/SPY?p=SPY')
     soup = bs4.BeautifulSoup(r.text,"lxml")
     price = soup.find_all('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-    return price
-#    r.close()
+    r = requests.get('https://finance.yahoo.com/quote/SPY/options?p=SPY')
     
-def getStrike():
-    strikePrice = 0
-    counter = -1
     while strikePrice!=input_strike:
         counter = counter + 1
-        r = requests.get('https://finance.yahoo.com/quote/SPY/options?p=SPY')
         soup = bs4.BeautifulSoup(r.text,"lxml")
         strikePrice = soup.find_all('a',{'class': 'C($linkColor) Fz(s)'})[counter].text
+        
     else:
-         r = requests.get('https://finance.yahoo.com/quote/SPY/options?p=SPY')
-         soup = bs4.BeautifulSoup(r.text,"lxml")
-         optionPrice = soup.find_all('td',{'class': 'data-col3 Ta(end) Pstart(7px)'})[counter].text
-         return strikePrice,optionPrice
+        optionPrice = soup.find_all('td',{'class': 'data-col3 Ta(end) Pstart(7px)'})[counter].text
+        return price,strikePrice,optionPrice
 
 while True:
-    a, b = getStrike()
     print ('The current SPY price is: ' + str(getPrice()))
-    print ('The current SPY strike price is: ' + str(getStrike()))
-    print ('The current SPY option price for this strike is: ' + str(getStrike()))    
-    time.sleep(30.0 - ((time.time() - starttime) % 30.0))
+    time.sleep(15.0 - ((time.time() - starttime) % 15.0))
